62167edd36150ccfee7509300b904810b6180c57
Damian97B <damian497k@gmail.com>
2022-09-17 20:48:22 +0200

    insert to database

--- index.php
+++ index.php
@@ -4,21 +4,35 @@ declare(strict_types=1);
 
 namespace App;
 
-require_once ('src/utils/debug.php');
-require_once('src/View.php');
-$test = 'test';
-// dump($test);
-$action = $_GET['action']?? null;
-// równoznaczne z tym powyższym
-// if (!empty($_GET['action'])){
-//     $action = $_GET['action'];
-// } else {
-//     $action = null;
-// }
+use App\Exception\AppException;
+use App\Exception\ConfigurationException;
+use Throwable;
 
-$view = new View();
-$view->render($action);
-// dump($view); 
+require_once("src/Utils/debug.php");
+require_once("src/Controller.php");
+require_once("src/Exception/AppException.php");
 
+$configuration = require_once("config/config.php");
 
-?>
+$request = [
+  'get' => $_GET,
+  'post' => $_POST
+];
+
+try {
+  //$controller = new Controller($request);
+  //$controller->run();
+
+  Controller::initConfiguration($configuration);
+  (new Controller($request))->run();
+} catch (ConfigurationException $e) {
+  //mail('xxx@xxx.com', 'Errro', $e->getMessage());
+  echo '<h1>Wystąpił błąd w aplikacji</h1>';
+  echo 'Problem z applikacją, proszę spróbować za chwilę.';
+} catch (AppException $e) {
+  echo '<h1>Wystąpił błąd w aplikacji</h1>';
+  echo '<h3>' . $e->getMessage() . '</h3>';
+} catch (Throwable $e) {
+  echo '<h1>Wystąpił błąd w aplikacji</h1>';
+  dump($e);
+}
--- src/View.php
+++ src/View.php
@@ -4,12 +4,8 @@ declare(strict_types=1);
 namespace App;
 
 class View{
-    public function render(?string $page): void{
+    public function render(string $page, array $params): void{
+        
         include_once('templates/layout.php');
-        if ($page === 'create'){
-            include_once ('templates/Pages/create.php');
-        } else {
-            include_once ('templates/Pages/list.php');
-        }
     }
 }
